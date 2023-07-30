396d6f3eca8d8930acbf6c66eb0e4be3018956f1
ikozlov55 <ilya621@yandex.ru>
2023-07-30 19:53:38 +0300

    Chapter 17 exercises 3 - 6

--- src/exercises/ch17/Chapter17.java
+++ src/exercises/ch17/Chapter17.java
@@ -1,7 +1,12 @@
 package exercises.ch17;
 
+import exercises.ch17.ex6.Loan;
+
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Scanner;
 
 public class Chapter17 {
     /*
@@ -69,24 +74,92 @@ public class Chapter17 {
     }
 
     /*
+        (Convert a text file into UTF) Write a program that reads lines of characters from
+        a text file and writes each line as a UTF string into a binary file. Display the
+        sizes of the text file and the binary file. Use the following command to run the
+        program:
+        java Exercise17_04 Welcome.java Welcome.utf
 
+        src/exercises/ch17/Chapter17.java src/exercises/ch17/data/Exercise17_4.utf
      */
-    public static void ch17_4() {
+    public static void ch17_4(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Invalid input!");
+            System.exit(1);
+        }
+        File input = new File(args[0]);
+        File output = new File(args[1]);
+        try (
+                Scanner scanner = new Scanner(input);
+                FileOutputStream out = new FileOutputStream(output);
+                DataOutputStream stream = new DataOutputStream(out)
+        ) {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                stream.writeUTF(line);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
 
+        System.out.printf("%s size is: %d\n", input.getName(), input.length());
+        System.out.printf("%s size is: %d\n", output.getName(), output.length());
     }
 
     /*
-
+        (Store objects and arrays in a file) Write a program that stores an array of the five
+        int values 1, 2, 3, 4, and 5, a Date object for the current time, and the double
+        value 5.5 into the file named Exercise17_05.dat. In the same program, write the
+        code to read and display the data.
      */
     public static void ch17_5() {
+        File file = new File("src/exercises/ch17/data/Exercise17_05.dat");
+        try (ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(file))) {
+            stream.writeObject(new int[]{1, 2, 3, 4, 5});
+            stream.writeObject(new Date());
+            stream.writeDouble(5.5);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
 
+        try (ObjectInputStream stream = new ObjectInputStream(new FileInputStream(file))) {
+            int[] array = (int[]) stream.readObject();
+            Date date = (Date) stream.readObject();
+            double value = stream.readDouble();
+            System.out.println(Arrays.toString(array));
+            System.out.println(date);
+            System.out.println(value);
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     /*
-
+        (Store Loan objects) The Loan class in Listing 10.2 does not implement
+        Serializable. Rewrite the Loan class to implement Serializable. Write
+        a program that creates five Loan objects and stores them in a file named
+        Exercise17_06.dat.
      */
     public static void ch17_6() {
+        File file = new File("src/exercises/ch17/data/Exercise17_05.dat");
+        try (ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(file))) {
+            for (int i = 1; i <= 5; i++) {
+                Loan loan = new Loan(5, i, 100_000);
+                stream.writeObject(loan);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
 
+        try (ObjectInputStream stream = new ObjectInputStream(new FileInputStream(file))) {
+            while (true) {
+                Loan loan = (Loan) stream.readObject();
+                System.out.println(loan);
+            }
+        } catch (EOFException ignored) {
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     /*
