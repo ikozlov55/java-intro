31363a17bdb0af0924a12350e8055afc15fc4be5
ikozlov55 <ilya621@yandex.ru>
2023-11-12 15:02:16 +0300

    Chapter 22 exercises 4-6

--- src/exercises/ch22/Chapter22.java
+++ src/exercises/ch22/Chapter22.java
@@ -1,9 +1,8 @@
 package exercises.ch22;
 
-import java.util.ArrayList;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Chapter22 {
     private static final Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
@@ -106,28 +105,187 @@ public class Chapter22 {
         } else {
             System.out.println("no match");
         }
-
     }
 
     /*
-
+        (Revise Boyer-Moore algorithm) Revise the implementation for the
+        Boyer-Moore algorithm in Listing 22.15 StringMatchBoyerMoore.java
+        to test where a mismatch character is in the pattern in O(1) time using a
+        set that consists of all the characters in the pattern. If the test is false, the algo-
+        rithm can shift the pattern past the mismatched character.
+            Enter a string s1: Mississippi
+            Enter a string s2: sip
+            matched at index 6
      */
     public static void ch22_4() {
+        System.out.print("Enter a string s1: ");
+        String s1 = scanner.nextLine();
+        System.out.print("Enter a string s2: ");
+        String s2 = scanner.nextLine();
+        int index = match(s1, s2);
+        if (index >= 0) {
+            System.out.printf("matched at index %d\n", index);
+        } else {
+            System.out.println("no match");
+        }
+    }
 
+    // Return the index of the first match. –1 otherwise.
+    public static int match(String text, String pattern) {
+        Set<Character> patternChars = new HashSet<>();
+        pattern.chars().forEach(x -> patternChars.add((char) x));
+        int i = pattern.length() - 1;
+        while (i < text.length()) {
+            int k = i;
+            int j = pattern.length() - 1;
+            while (j >= 0) {
+                if (text.charAt(k) == pattern.charAt(j)) {
+                    k--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+            if (j < 0) {
+                return i - pattern.length() + 1; // A match found
+            }
+            if (!patternChars.contains(text.charAt(k))) {
+                i = k + pattern.length();
+            } else {
+                i = k + pattern.length() - 1 - findLastIndex(text.charAt(k), j - 1, pattern);
+            }
+        }
+
+        return -1;
+    }
+
+    // Return the index of the last element in pattern[0 .. j]
+    // that matches ch. –1 otherwise.
+    private static int findLastIndex(char ch, int j, String pattern) {
+        for (int k = j; k >= 0; k--) {
+            if (ch == pattern.charAt(k)) {
+                return k;
+            }
+        }
+        return -1;
     }
 
     /*
-
+        (Same-number subsequence) Write an O(n) time program that prompts the user
+        to enter a sequence of integers ending with 0 and finds the longest subsequence
+        with the same number. Here is a sample run of the program:
+            Enter a series of numbers ending with 0:
+            2 4 4 8 8 8 8 2 4 4 0
+            The longest same number sequence starts at index 3 with 4 values of 8
      */
     public static void ch22_5() {
-
+        System.out.println("Enter a series of numbers ending with 0:");
+        ArrayList<Integer> numbers = new ArrayList<>();
+        while (true) {
+            int n = scanner.nextInt();
+            if (n == 0) {
+                break;
+            } else {
+                numbers.add(n);
+            }
+        }
+        int index = -1;
+        int number = 0;
+        int sequenceLength = 0;
+        int maxNumber = 0;
+        int maxSequenceLength = 0;
+        int maxIndex = -1;
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) == number) {
+                sequenceLength++;
+            } else {
+                if (sequenceLength > maxSequenceLength) {
+                    maxSequenceLength = sequenceLength;
+                    maxNumber = number;
+                    maxIndex = index;
+                }
+                number = numbers.get(i);
+                sequenceLength = 1;
+                index = i;
+            }
+        }
+        System.out.printf("The longest same number sequence starts at index %d with %d values of %d\n",
+                maxIndex, maxSequenceLength, maxNumber);
     }
 
     /*
-
+        (Execution time for GCD) Write a program that obtains the execution time for
+        finding the GCD of every two consecutive Fibonacci numbers from the index
+        40 to index 45 using the algorithms in Listing 22.3 and Listing 22.4. Your pro-
+        gram should print a table like this:
      */
     public static void ch22_6() {
+        List<Integer> fibIndexes = IntStream.rangeClosed(40, 45).boxed().toList();
+        Map<Integer, Long> fibValues = new HashMap<>();
+        for (int n : fibIndexes) {
+            fibValues.put(n, fib(n));
+        }
+        List<Double> gcdResults = new ArrayList<>();
+        List<Double> gcdEuclidResults = new ArrayList<>();
+        for (int i = 0; i < fibIndexes.size() - 1; i++) {
+            long m = fibValues.get(fibIndexes.get(i + 1));
+            long n = fibValues.get(fibIndexes.get(i));
+            gcdResults.add(executionTime(() -> gcd(m, n)));
+            gcdEuclidResults.add(executionTime(() -> gcdEuclid(m, n)));
+        }
 
+        String fibsHeader = String.join("", fibIndexes.stream().map(x -> String.format("%8d", x)).toList());
+        System.out.printf("%s│%s\n", " ".repeat(25), fibsHeader);
+        System.out.printf("%s┼%s\n", "─".repeat(25), "─".repeat(fibIndexes.size() * 10));
+        String gcdRow = String.join("", gcdResults.stream().map(x -> String.format("%8.2f", x)).toList());
+        System.out.printf("%-25s│%s\n", "Listing 22.3 GCD", gcdRow);
+        String gcdEuclidRow = String.join("", gcdEuclidResults.stream().map(x -> String.format("%8.2f", x)).toList());
+        System.out.printf("%-25s│%s\n", "Listing 22.3 GCDEuclid", gcdEuclidRow);
+    }
+
+    private static double executionTime(Runnable action) {
+        long startTime = System.currentTimeMillis();
+        action.run();
+        long endTime = System.currentTimeMillis();
+        return (endTime - startTime) / 1000.0;
+    }
+
+    public static long gcd(long m, long n) {
+        long gcd = 1;
+        if (m % n == 0) return n;
+        for (long k = n / 2; k >= 1; k--) {
+            if (m % k == 0 && n % k == 0) {
+                gcd = k;
+                break;
+            }
+        }
+
+        return gcd;
+    }
+
+    public static long gcdEuclid(long m, long n) {
+        return m % n == 0 ? n : gcdEuclid(n, m % n);
+    }
+
+    public static long fib(long n) {
+        long f0 = 0; // For fib(0)
+        long f1 = 1; // For fib(1)
+        long f2 = 1; // For fib(2)
+
+        if (n == 0)
+            return f0;
+        else if (n == 1)
+            return f1;
+        else if (n == 2)
+            return f2;
+
+        for (int i = 3; i <= n; i++) {
+            f0 = f1;
+            f1 = f2;
+            f2 = f0 + f1;
+        }
+
+        return f2;
     }
 
     /*
